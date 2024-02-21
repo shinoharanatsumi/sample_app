46b8ef4ee3a52744ef316802d2f47e52239554dc
EC2 Default User <ec2-user@ip-172-31-26-77.ap-northeast-1.compute.internal>
2024-02-21 12:58:22 +0000

    [Finish] 3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,10 +5,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
-  end
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+    end
 
   def index
     @lists = List.all
